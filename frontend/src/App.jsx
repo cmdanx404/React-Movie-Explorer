6448f2358be80174c68ffeecc1a639c9b97171eb
danielmadjus <danielmadjus@gmail.com>
2025-10-08 23:25:50 +0800

    upload complete initial files-

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,22 @@
-import './App.css'
+import "./css/App.css";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <>
-      <Text display="What's up"/>
-      <Text display="Hello"/>
-    </>
-      
-  )
-}
-function Text({display}) {
-  return (
-    <div>
-      <p>{display}</p>
-    </div>
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
   );
 }
-export default App
+
+export default App;
